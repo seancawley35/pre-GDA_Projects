a6a1cdda2fe394d2e7de61b6b247300775cd711a
seancawley35 <48701193+seancawley35@users.noreply.github.com>
2022-08-11 16:48:01 -0700

    Update oddeven.java
    Given an integer number, output Odd if it is odd, or Even if it is even.
    
    Input
    The input contains exatly one number N.
    
    Output
    Output one a single line containing Odd or Even.

--- week02/oddeven.java
+++ week02/oddeven.java
@@ -1,3 +1,15 @@
+/*
+Given an integer number, output Odd if it is odd, or Even if it is even.
+
+Input
+The input contains exatly one number N.
+
+Output
+Output one a single line containing Odd or Even.
+
+*/
+
+
 import java.util.Scanner;
 
 
