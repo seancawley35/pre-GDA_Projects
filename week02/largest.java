d7560cc81b85bc83692450a7d59a81e5e776e3c4
seancawley35 <48701193+seancawley35@users.noreply.github.com>
2022-08-11 16:49:19 -0700

    Update largest.java

--- week02/largest.java
+++ week02/largest.java
@@ -1,3 +1,16 @@
+
+/*
+Order Three Numbers
+Given three different integer numbers, output them in the ascending order.
+
+Input
+The input contains three integer numbers.
+
+Output
+Output the same numbers in the ascending order.
+
+*/
+
 import java.util.Scanner;
 
 
